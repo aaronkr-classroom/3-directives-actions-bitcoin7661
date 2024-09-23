3843d3bf492e149d7f1c064c47068c952b03eb1e
bitcoin7661 <wotjd7661@gmail.com>
2024-09-23 23:12:53 +0900

    보강 실습

---
+++ src/main/java/ch04/com/dao2/Person2.java
@@ -0,0 +1,18 @@
+package ch04.com.dao2;
+
+public class Person2 {
+	private int id=20201506;
+	private String name ="홍길순";
+	
+	public Person2() {}
+	
+	public int getId() {return id; }
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {return name;} 
+	public void setName(String name) {
+		this.name=name;
+	}
+}
---
+++ src/main/webapp/0403_flush.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>flush 액션 태그</title>
+</head>
+<body>
+	<h3> 이 파일은 0403_flush.jsp입니다</h3>
+	<jsp:include page="0404_flush.jsp" flush>
+	<p>Jakarta Server Page</p>
+
+</body>
+</html>
